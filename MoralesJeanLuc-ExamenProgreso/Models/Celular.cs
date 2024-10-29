852bd8c526fde3b1ce33679a3e3f759e286e3cd1
JeanMV52 <jeanmorales24@outlook.com>
2024-10-29 07:45:40 -0500

    Creación de Clase MoralesJeanLuc y Clase Celular dentro de los modelos.

---
+++ MoralesJeanLuc-ExamenProgreso/Models/Celular.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoralesJeanLuc_ExamenProgreso.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(200)]
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        [Range(1.0, 3000.0, ErrorMessage = "Ingrese un valor entre 1.0 y 3000.0")]
+        public decimal Precio { get; set; }
+        public MoralesJeanLuc Dueño { get; set; }
+        [ForeignKey("MoralesJeanLuc")]
+        public int IdMoralesJeanLuc { get; set; }
+        
+    }
+}
---
+++ MoralesJeanLuc-ExamenProgreso/Models/MoralesJeanLuc.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoralesJeanLuc_ExamenProgreso.Models
+{
+    public class MoralesJeanLuc
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required]
+        [Range(1.0,5000.0,ErrorMessage = "Ingrese un valor entre 1.0 y 5000.0")]
+        public decimal Sueldo { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+        [Required]
+        public bool HorasExtra { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Inicio de contrato")]
+        public DateTime FechaInicio { get; set; }
+    }
+}
