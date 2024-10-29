2557d36fd5fbcbefa0348f2568c6811155795e96
JeanMV52 <jeanmorales24@outlook.com>
2024-10-29 07:25:22 -0500

    Agregar archivos de proyecto.

---
+++ MoralesJeanLuc-ExamenProgreso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoralesJeanLuc_ExamenProgreso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
